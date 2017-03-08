97c6e2543fe1d7285a098d58ad7cc3e1ae40605f
kelvinrandu <randukelvin@gmail.com>
2017-03-09 00:21:13 +0300

    solution:  Super sum

--- issue-007/solution.js
+++ issue-007/solution.js
@@ -1,4 +1,15 @@
 // implement your solution here
 function superSum() {
+  var sum = 0;
+a.forEach(function(entry) {
+
+    while(entry > 0)
+    {
+        sum += entry%10;
+        entry = Math.floor(entry/10);
+    }
+  //  console.log(sum);
+});
+    return sum;
 
 }
