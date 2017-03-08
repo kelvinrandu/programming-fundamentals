ae1446e057f3b2fe18877ebe25620830b8556521
kelvinrandu <randukelvin@gmail.com>
2017-03-08 23:29:59 +0300

    solution:  product[C

--- issue-003/solution.js
+++ issue-003/solution.js
@@ -1,3 +1,13 @@
 function product(a, b) {
     // implement your solution here
+    try {
+     if(a == ""|| b == "") throw "NaN exception";
+     if(isNaN(b) || isNaN(a)) throw "NaN exception";
+     a = Number(a);
+     b = Number(b);
+     return a*b;
+ }
+ catch(err) {
+     console.log(err);
+ }
 }
